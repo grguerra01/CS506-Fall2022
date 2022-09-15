a9fd757da628e84603851bf272b30e5a10d5468e
grguerra01 <98914563+grguerra01@users.noreply.github.com>
2022-09-15 18:02:57 -0400

    Add files via upload
    Lab01 done by Galo Guerra, Kanishka Gupta, and Xiang Li U53450247

--- labs/01-lab/leisure/restaurant.py
+++ labs/01-lab/leisure/restaurant.py
@@ -1,3 +1,5 @@
+from PIL import Image
+
 def draw_restaurant():
-    print("restaurant not found")
-    return
+    img = Image.open("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcontent.jwplatform.com%2Fthumbs%2FCmUamLMh-1280.jpg&f=1&nofb=1")
+    img.show
